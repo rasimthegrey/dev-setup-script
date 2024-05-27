2a7cd76c811b391f70edfb67f9016318b81f61c4
Rasim Mutlu <rasim.mutlu99@gmail.com>
2024-05-27 12:27:55 +0300

    added error check mechanism for commands

--- setup-environment.sh
+++ setup-environment.sh
@@ -1,33 +1,81 @@
 #!/bin/bash
 
-# Update packages:
-sudo apt-get update > /dev/null 2>&1
+# color palette
+green_prefix="\033[32m" # success
+blue_prefix="\033[34m" # processing
+red_prefix="\033[31m" # error
+suffix="\033[00m" #suffix
 
 
-echo -e "\033[34m----------\nINSTALLING DOCKER...\n----------\033[0m" 
-# Add Docker's GPG key:
-sudo apt-get install -y ca-certificates curl > /dev/null 2>&1
-sudo install -m 0755 -d /etc/apt/keyrings > /dev/null 2>&1
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc > /dev/null 2>&1
-sudo chmod a+r /etc/apt/keyrings/docker.asc  > /dev/null 2>&1
+install_docker() {
+    # Update packages:
+    echo -e "$blue_prefix"Updating packages..."$suffix"
+    sudo apt-get update > /dev/null 2> errors.log
+    if [ $? -eq 0 ]; then
+        echo -e "$green_prefix"Packages updated."$suffix"
+    else
+        echo -e "$red_prefix"An error occured while updating packages. Error details have been logged to errors.log."$suffix"
+    fi
 
-# Add repository to apt sources:
-echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update  > /dev/null 2>&1
+    # Add Docker's GPG key:
+    echo -e "$blue_prefix"Installing ca-certificates and curl..."$suffix"
+    sudo apt-get install -y ca-certificates curl > /dev/null 2> errors.log
+    if [ $? -eq 0 ]; then
+        echo -e "$green_prefix"Installation successful."$suffix"
+    else
+        echo -e "$red_prefix"An error occured while installing. Error details have been logged to errors.log."$suffix"
+    fi
 
-# Install the latest Docker packages:
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null 2>&1
+    sudo install -m 0755 -d /etc/apt/keyrings > /dev/null 2> errors.log
 
-# Enable and start Docker service:
-sudo systemctl enable docker > /dev/null 2>&1
-sudo systemctl start docker > /dev/null 2>&1
-echo -e "\033[32m----------\nDONE\n----------\033[0m"
+    echo -e "$blue_prefix"Downloading and saving GPG key for Docker..."$suffix" 
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc > /dev/null 2>errors.log
+    if [ $? -eq 0 ]; then
+        echo -e "$green_prefix"Successful."$suffix"
+    else
+        echo -e "$red_prefix"An error occured while downloading GPG key. Error details have been logged to errors.log."$suffix"
+    fi
+    sudo chmod a+r /etc/apt/keyrings/docker.asc  > /dev/null 2>&1
+
+    # Add repository to apt sources:
+    echo -e "$blue_prefix"Adding repository to apt sources..."$suffix"
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    if [ $? -eq 0 ]; then
+        echo -e "$green_prefix"Repository added."$suffix"
+    else
+        echo -e "$red_prefix"Repository could not added to apt sources."$suffix"
+    fi
+
+    echo -e "$blue_prefix"Updating packages..."$suffix"
+    sudo apt-get update  > /dev/null 2> errors.log
+    if [ $? -eq 0 ]; then
+        echo -e "$green_prefix"Packages updated."$suffix"
+    else
+        echo -e "$red_prefix"An error occured while updating packages. Error details have been logged to errors.log."$suffix"
+    fi
+
+    # Install the latest Docker packages:
+    echo -e "$blue_prefix"Installing Docker packages..."$suffix"
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null 2> errors.log
+    if [ $? -eq 0 ]; then
+        echo -e "$green_prefix"Installed Docker successfully."$suffix"
+    else
+        echo -e "$red_prefix"An error occured while installing Docker. Error details have been logged to errors.log."$suffix"
+    fi
+
+    # Enable and start Docker service:
+    sudo systemctl enable docker > /dev/null 2>&1
+    sudo systemctl start docker > /dev/null 2>&1
+}
+install_docker
 
 
-echo -e "\033[34m----------\nSETTING UP DEV ENVIRONMENT...\n----------\033[0m" 
+# echo -e "\033[34m----------\nSETTING UP DEV ENVIRONMENT...\n----------\033[0m" 
+
+
 # Create project directories
 mkdir -p ~/dev-env/
 mkdir -p ~/dev-env/nginx
@@ -38,7 +86,7 @@ git clone https://github.com/vuejs/v2.vuejs.org.git
 cd ..
 
 # Create Vue.js (Node.js) Dockerfile:
-cat <<EOL > ~dev-env/v2.vuejs.org/Dockerfile
+cat <<EOL > ~/dev-env/v2.vuejs.org/Dockerfile
 FROM node:18
 WORKDIR /app
 COPY package.json yarn.lock ./
@@ -85,8 +133,13 @@ services:
     depends_on:
       - app
 EOL
-echo -e "\033[32m----------\nDONE\n----------\033[0m"
-echo -e "\033[34m----------\nSTARTING DEV ENVIRONMENT...\n----------\033[0m" 
+
+
+# echo -e "\033[32m----------\nDONE\n----------\033[0m"
+# echo -e "\033[34m----------\nSTARTING DEV ENVIRONMENT...\n----------\033[0m" 
+
+
 # Start development environment
 cd ~/dev-env
 sudo docker-compose up -d
+sudo docker-compose down
